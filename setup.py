5cf4527ddf84696c277b0318edffe9e546b61561
chenle02 <chenle02@gmail.com>
2024-01-25 09:07:20 -0600

    Update version to 1.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simulation_super_brownian_motions',
-    version='V1.2',
+    version='1.2.1',
     packages=find_packages(),
     description='This is a package for simulating super Brownian motions.',
     long_description=open('README.md').read(),
